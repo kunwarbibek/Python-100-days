02f9796452fe5120c732bd463320cdb2e5492254
Dark Shadoow <kunwarbibek66632@gmail.com>
2025-11-09 20:23:15 +0545

    Python variable number operators and data types

---
+++ day1.py
@@ -0,0 +1,54 @@
+# variable in python 
+x = 4
+print(x)
+print(type(x))
+
+x="4";
+print(x)
+print(type(x))
+
+x="Dark shadoow"
+print("My Gaming name is ",x)
+
+print("***********************************************")
+
+# *****************Data Types **********************
+x=1
+print(type(x))
+
+y=1.1
+print(type(y))
+
+z=1j;
+print(type(z))
+
+print("***********************************************")
+
+# *****************operators  **********************
+# arthametic 
+x = 10
+y = 3
+print("Arithmetic Operators:")
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x // y =", x // y)
+print("x % y =", x % y)
+print("x ** y =", x ** y)
+
+#print("Comparison Operators:")
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y:", x > y)
+print("x < y:", x < y)
+print("x >= y:", x >= y)
+print("x <= y:", x <= y)
+
+# loical operators
+a = True
+b = False
+print("Logical Operators:")
+print("a and b:", a and b)
+print("a or b:", a or b)
+print("not a:", not a)
